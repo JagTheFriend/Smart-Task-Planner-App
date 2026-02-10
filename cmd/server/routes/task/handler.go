e5029438762221217925cd097f4fd69f3a1a809d
JagTheFriend <JagTheFriend12@gmail.com>
2026-02-10 22:34:24 +0530

    Created custom middleware to handle JWT auth

--- cmd/server/routes/task/handler.go
+++ cmd/server/routes/task/handler.go
@@ -1,6 +1,9 @@
 package task
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo/v5"
 	"gorm.io/gorm"
 )
@@ -13,8 +16,30 @@ type TaskHandler struct {
 func NewTaskHandler(e *echo.Group, db *gorm.DB) *TaskHandler {
 	authGroup := e.Group("/task")
 
+	authGroup.Use(GetJwtDataMiddleware)
+
 	return &TaskHandler{e: authGroup, db: db}
 }
 
 func (h *TaskHandler) RegisterRoutes() {
+	h.e.POST("", h.createTask)
+	h.e.GET("", h.getTasks)
+	h.e.PUT("", h.updateTask)
+	h.e.DELETE("", h.deleteTask)
+}
+
+func (h *TaskHandler) createTask(c *echo.Context) error {
+	return c.String(http.StatusOK, fmt.Sprintf("%d", c.Get("UserId")))
+}
+
+func (h *TaskHandler) getTasks(c *echo.Context) error {
+	return nil
+}
+
+func (h *TaskHandler) updateTask(c *echo.Context) error {
+	return nil
+}
+
+func (h *TaskHandler) deleteTask(c *echo.Context) error {
+	return nil
 }
