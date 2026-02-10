1391eeef4661b9de05ec61780ffc16f5bd12e029
JagTheFriend <JagTheFriend12@gmail.com>
2026-02-10 20:05:30 +0530

    Setup authentication routes

---
+++ cmd/server/routes/authentication/handler.go
@@ -0,0 +1,30 @@
+package authentication
+
+import (
+	"github.com/labstack/echo/v5"
+	"gorm.io/gorm"
+)
+
+type AuthHandler struct {
+	e  *echo.Group
+	db *gorm.DB
+}
+
+func NewAuthHandler(e *echo.Group, db *gorm.DB) *AuthHandler {
+	authGroup := e.Group("/auth")
+
+	return &AuthHandler{e: authGroup, db: db}
+}
+
+func (h *AuthHandler) RegisterRoutes() {
+	h.e.POST("/login", h.signup)
+	h.e.POST("/login", h.login)
+}
+
+func (h *AuthHandler) signup(c *echo.Context) error {
+	return nil
+}
+
+func (h *AuthHandler) login(c *echo.Context) error {
+	return nil
+}
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,19 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"smart-task-planner/cmd/server/routes/authentication"
+	"smart-task-planner/internal/database"
 
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 )
 
 func StartServer() {
+	db := database.Connect()
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", func(c *echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	apiGroup := e.Group("/api/v1")
+	apiGroup.GET("/health", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"health": "ok"})
 	})
+
+	authHandler := authentication.NewAuthHandler(apiGroup, db)
+	authHandler.RegisterRoutes()
 
 	if err := e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))); err != nil {
 		e.Logger.Error("failed to start server", "error", err)
